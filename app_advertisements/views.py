aaa98633c470d97261a4a15f5544ca861ec645d8
vsevolodfilippov <vsevolodfilippov@yandex.ru>
2023-08-24 14:26:11 +0300

    lesson11

---
+++ app_advertisements/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Advertisement
+from .forms import AdvertisementForm
+
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'app_advertisements/index.html', context)
+
+
+def top_sellers(request):
+    return render(request, 'app_advertisements/top-sellers.html')
+
+
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_advertisement = form.save(commit=False)
+            new_advertisement.user = request.user
+            new_advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'app_advertisements/advertisement-post.html', context)
